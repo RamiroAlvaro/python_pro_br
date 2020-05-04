94b4c0588a3357a1e3e864cdf8143db9b60ee304
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-04 19:32:44 -0300

    Utilizando bloques de template y variables de contexto

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {
+        'titulo': 'Video Aperitivo: Motivação',
+        'vimeo_id': 251224475
+    }
+    return render(request, 'aperitivos/video.html', context={'video': video})

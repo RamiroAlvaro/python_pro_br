dc36eaa05937fd46d1e622058a1c19a7308f281b
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-20 23:20:44 -0300

    Implementando pagina de login.

--- pypro/urls.py
+++ pypro/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pypro.base.urls')),
+    path('contas/', include('django.contrib.auth.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
     path('turmas/', include('pypro.turmas.urls')),
